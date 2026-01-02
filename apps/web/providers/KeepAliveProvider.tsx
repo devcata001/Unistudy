1265f8a94bf04b98d4565598e054f61438985280
devcata001 <danieladeyi001@gmail.com>
2026-01-02 15:08:53 +0100

    Trigger Vercel redeploy - add keep-alive console log

--- apps/web/providers/KeepAliveProvider.tsx
+++ apps/web/providers/KeepAliveProvider.tsx
@@ -5,7 +5,8 @@ import { keepAliveService } from '@/lib/keepAlive'
 
 export function KeepAliveProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
-        // Start keep-alive service when app mounts
+        // Start keep-alive service when app mounts to prevent backend sleeping
+        console.log('[KeepAliveProvider] Initializing keep-alive service...')
         keepAliveService.start()
 
         // Cleanup on unmount
